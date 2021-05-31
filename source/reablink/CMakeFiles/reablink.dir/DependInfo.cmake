7de687be8ba1c7f7f1008a53896aff7eea4bba92
ak5k <en5ca84@gmail.com>
2021-05-31 13:10:10 +0300

    dev

---
+++ source/reablink/CMakeFiles/reablink.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Antti/source/repos/reablink/source/reablink/BlinkEngine.cpp" "/mnt/c/Users/Antti/source/repos/reablink/source/reablink/CMakeFiles/reablink.dir/BlinkEngine.cpp.o"
+  "/mnt/c/Users/Antti/source/repos/reablink/source/reablink/ReaBlink.cpp" "/mnt/c/Users/Antti/source/repos/reablink/source/reablink/CMakeFiles/reablink.dir/ReaBlink.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINK_PLATFORM_LINUX=1"
+  "LINK_PLATFORM_UNIX=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "vendor/reaper-sdk/sdk"
+  "include"
+  "vendor/link/include"
+  "vendor/link/cmake_include/../modules/asio-standalone/asio/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
