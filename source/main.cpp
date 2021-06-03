cabfae538f630217a1dfc55883a7615a4e415275
ak5k <en5ca84@gmail.com>
2021-06-03 04:17:20 +0300

    dev

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,3 @@
-// #if defined(_WIN32) && !defined(NDEBUG)
-// // debug_new.cpp
-// // compile by using: cl /EHsc /W4 /DNDEBUG /MDd debug_new.cpp
-// #define _CRTDBG_MAP_ALLOC
-// #include <crtdbg.h>
-// #include <cstdlib>
-// #ifndef NDEBUG
-// #define DBG_NEW new (_NORMAL_BLOCK, __FILE__, __LINE__)
-// // Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
-// // allocations to be of _CLIENT_BLOCK type
-// #else
-// #define DBG_NEW new
-// #endif
-// #endif
-
 #ifdef NDEBUG
 #define REAPERAPI_MINIMAL
 #define REAPERAPI_WANT_Audio_RegHardwareHook
@@ -40,9 +25,9 @@
 #define REAPERAPI_WANT_time_precise
 #endif
 
-#include "reablink/ReaBlink.hpp"
 #define REAPERAPI_IMPLEMENT
 #include <reaper_plugin_functions.h>
+#include "reablink/ReaBlink.hpp"
 
 REAPER_PLUGIN_HINSTANCE g_hInst; // to api extern
 
@@ -56,9 +41,6 @@ REAPER_PLUGIN_DLL_EXPORT int ReaperPluginEntry(
         return 1;
     }
     else {
-        // #if defined(_WIN32) && !defined(NDEBUG)
-        //         _CrtDumpMemoryLeaks();
-        // #endif
         unregisterReaBlink();
         return 0;
     }
